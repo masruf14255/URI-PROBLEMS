27345d3f5aee55e3ee3ae81f2f61262441dbd3c4
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2020-11-22 01:20:00 -0800

    Create 1008.cpp.c

---
+++ 1008.cpp.c
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,t;
+    float s,SALARY;
+
+    cin>>n>>t>>s;
+
+    SALARY=t*s;
+
+    cout<<"NUMBER = "<<n<<endl;
+    cout<<fixed<<setprecision(2)<<"SALARY = U$ "<<SALARY<<endl;
+    return 0;
+
+}
