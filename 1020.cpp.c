0cb16b400836f5dc92cada18f36f859d686c09b4
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2020-11-22 01:21:49 -0800

    Create 1020.cpp.c

---
+++ 1020.cpp.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    long long int input,temp,year,month;
+    cin >> input;
+    year = input / 365;
+    temp = input % 365;
+    month = temp / 30;
+    temp = temp % 30;
+    cout<< year <<" ano(s)" << endl;
+    cout<< month <<" mes(es)" << endl;
+    cout<< temp <<" dia(s)" << endl;
+    return 0;
+}
