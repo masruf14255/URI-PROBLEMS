cda8e6a2faeee5f7a14b16895effa393da9ea96a
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-23 10:02:11 +0600

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,18 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double a,b,c,n;
+	cin>>a>>b>>c;
+	a=a*2;
+	b=b*3;
+	c=c*5;
+	n=(a+b+c)/10.0;
+	printf("MEDIA = %.1lf\n",n);
+}
