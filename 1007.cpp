6466846d1028dcc246f1409129aaf994a934f5f6
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-23 10:05:10 +0600

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,16 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b,c,d,m=0;
+	cin>>a>>b>>c>>d;
+	m=((a*b)-(c*d));
+	printf("DIFERENCA = %d\n",m);
+
+}
