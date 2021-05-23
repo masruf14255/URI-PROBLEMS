a497c12e1b960387c2cfd17f8633d41d8fb14be8
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-23 10:11:11 +0600

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,18 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	char name[50];
+	int n;
+	float a,s,b;
+	cin>>n>>a>>b;
+	s=(a*b);
+	printf("NUMBER = %d\n",n);
+	printf("SALARY = U$ %.2f\n",s);
+}
