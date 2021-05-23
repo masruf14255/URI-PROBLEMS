6d481005e231d2500d1c861aeadd60a3507481ac
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-23 10:20:03 +0600

    Create 1009.cpp

---
+++ 1009.cpp
@@ -0,0 +1,17 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	double m,b,t;
+	cin>>s>>m>>b;
+	t=(b*15)/100;
+	printf("TOTAL = R$ %.2lf\n",t+m);
+
+}
