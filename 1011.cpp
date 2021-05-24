242da228e13600e5d844997b1f6d4acd1c672895
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-24 12:29:18 +0600

    Create 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,19 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double pi=3.14159,t,r,a;
+	cin>>r;
+	a=(4.0/3);
+	r=(r*r*r);
+	t=a*pi*r;
+	printf("VOLUME = %.3lf\n",t);
+
+
+}
