1cc09e1ba3b1323539cdf93767a76918cc4f97a0
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-24 14:09:25 +0600

    Create 1014.cpp

---
+++ 1014.cpp
@@ -0,0 +1,16 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b,t;
+    cin>>a>>b;
+    t=a/b;
+    printf("%.3f km/l\n",t);
+}
+
