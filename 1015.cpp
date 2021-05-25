cce1f3e7caae17aade983599a9eb9dea10a2892c
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-25 11:17:28 +0600

    Create 1015.cpp

---
+++ 1015.cpp
@@ -0,0 +1,19 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    double x1,x2,y1,y2,p,q,d;
+    cin>>x1>>y1>>x2>>y2;
+    d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    printf("%.4lf\n",d);
+
+}
+
+
