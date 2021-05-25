0873e64b921ed34a7c87c0d16accaa62e452e4e6
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-25 19:11:11 +0600

    Update 1036.cpp

--- 1036.cpp
+++ 1036.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a,b,c;
+    cin>>a>>b>>c;
+    if((pow(b,2)-4*a*c)<0 || 2*a==0)
+    {
+        cout<<"Impossivel calcular\n";
+    }
+    else
+    {
+        printf("R1 = %.5lf\n",((-b)+sqrt((pow(b,2)-4*a*c)))/(2*a));
+        printf("R2 = %.5lf\n",((-b)-sqrt((pow(b,2)-4*a*c)))/(2*a));
+    }
 
-int main(){
-    double a, b, c;
-
-    std::cin >> a >> b >> c;
-
-    if((pow(b,2) - 4*a*c) < 0 || 2*a == 0)
-                 std::cout << "Impossivel calcular\n";
-    else{
-         printf("R1 = %.5lf\n",((-b) + sqrt((pow(b,2) - 4*a*c)))/(2*a));
-         printf("R2 = %.5lf\n",((-b) - sqrt((pow(b,2) - 4*a*c)))/(2*a));
-         }
-    return 0;
 }
