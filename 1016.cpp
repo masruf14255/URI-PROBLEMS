5787aae7983834677706a4a793c2284937e8e142
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-25 11:17:31 +0600

    Create 1016.cpp

---
+++ 1016.cpp
@@ -0,0 +1,16 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n*2<<" minutos\n";
+
+}
