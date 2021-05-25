863a6916a9b0b649c2784c827e1b33aea5f5cf6c
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-25 11:57:07 +0600

    Update 1018.cpp

--- 1018.cpp
+++ 1018.cpp
@@ -8,31 +8,12 @@
 using namespace std;
 int main()
 {
-    int n;
-    int a,b,c,d,e,f;
+    int n,h,m;
     cin>>n;
-    cout<<n<<endl;
-    a=n/100;
-    n=n%100;
-    b=n/50;
-    n=n%50;
-    c=n/20;
-    n=n%20;
-    d=n/10;
-    n=n%10;
-    e=n/5;
-    n=n%5;
-    f=n/2;
-    n=n%2;
-    cout<<a<<" nota(s) de R$ 100,00\n";
-    cout<<b<<" nota(s) de R$ 50,00\n";
-    cout<<c<<" nota(s) de R$ 20,00\n";
-    cout<<d<<" nota(s) de R$ 10,00\n";
-    cout<<e<<" nota(s) de R$ 5,00\n";
-    cout<<f<<" nota(s) de R$ 2,00\n";
-    cout<<n<<" nota(s) de R$ 1,00\n";
-
-    return 0;
-    //cout<<a<<" "<<n<<endl;
+    h=n/3600;
+    n=n%3600;
+    m=n/60;
+    n=n%60;
+    cout<<h<<":"<<m<<":"<<n<<endl;
 
 }
