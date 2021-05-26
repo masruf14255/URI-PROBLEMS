9cf6ae074f1b86e03c08f277df4e17b70dd732aa
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 00:59:53 +0600

    Update 1038.cpp

--- 1038.cpp
+++ 1038.cpp
@@ -8,24 +8,46 @@
 using namespace std;
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    switch(n)
-    {
-    case 1:
-        printf("Total: R$ %.2lf\n",4.00*m);
-        break;
-    case 2:
-        printf("Total: R$ %.2lf\n",4.50*m);
-        break;
-    case 3:
-        printf("Total: R$ %.2lf\n",5.00*m);
-        break;
-    case 4:
-        printf("Total: R$ %.2lf\n",2.00*m);
-        break;
-    case 5:
-        printf("Total: R$ %.2lf\n",1.50*m);
-        break;
-    }
+
+     int a,b,c;
+     cin>>a>>b>>c;
+     if(a<b)
+     {
+     	if(a<c)
+     	{
+     		if(b<c)
+     		{
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",a,b,c,a,b,c);
+     		}
+     		else
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",a,c,b,a,b,c);
+
+     	}
+     }
+    if(b<c)
+     {
+     	if(b<a)
+     	{
+     		if(c<a)
+     		{
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",b,c,a,a,b,c);
+     		}
+     		else
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",b,a,c,a,b,c);
+     	}
+     }
+    if(c<a)
+     {
+     	if(c<b)
+     	{
+     		if(a<b)
+     		{
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",c,a,b,a,b,c);
+     		}
+     		else
+     			printf("%d\n%d\n%d\n\n%d\n%d\n%d\n",c,b,a,a,b,c);
+     	}
+     }
+
+
 }
