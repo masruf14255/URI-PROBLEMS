5df1b99197dc23a3e0f7c301fb7861ef878882db
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 22:04:30 +0600

    Create 1060.cpp

---
+++ 1060.cpp
@@ -0,0 +1,23 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+     float i=6,p;
+	while(i--)
+	{
+		float i;
+		cin>>i;
+		if(i>0)
+			p++;
+	}
+	cout<<p<<" valores positivos"<<endl;
+
+
+}
