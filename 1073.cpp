7b93018b43738e8da0c092c8fa6271157742f6ba
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 22:27:31 +0600

    Update 1073.cpp

--- 1073.cpp
+++ 1073.cpp
@@ -1,30 +1,23 @@
-#include<iostream>
-#include<stdio.h>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include <iostream>
+
 using namespace std;
 
 int main() {
 
+    int N;
+    cin>>N;
+    for(int i=1;i<=N;i++)
+    {
+        if(i%2==0)
+        cout<<i<<"^"<<2<<" = "<<i*i<<endl;
+    }
 
- int N, i, x;
- scanf("%d", &N);
- for( i = 1; i <= N; i++){
-  scanf("%d", &x);
-  if(x == 0){
-   printf("NULL\n");
-  }else if(x % 2 == 0){
-   if(x > 0){
-    printf("EVEN POSITIVE\n");
-   }else{
-    printf("EVEN NEGATIVE\n");
-   }
-
-  }else{
-   if(x > 0){
-    printf("ODD POSITIVE\n");
-   }else{
-    printf("ODD NEGATIVE\n");
-   }
-  }
-
- }
+    return 0;
 }
+
