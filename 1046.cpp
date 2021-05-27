27d1f18b4ad9eb55756ac53a3e264b8f113d6f79
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 21:27:13 +0600

    Update 1046.cpp

--- 1046.cpp
+++ 1046.cpp
@@ -1,17 +1,25 @@
-#include<iostream>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int a,b,c;
-    cin>>a>>b;
-    if(a>=b)
-    {
-        c=(24-a)+b;
-
-    }
-    else
-        c=(b-a);
-
-    cout<<"O JOGO DUROU "<<c<<" HORA(S)"<<endl;
-    return 0;
+	int a,b;
+	cin>>a>>b;
+	if(a==0 && b==0 || a==b)
+	{
+		cout<<"O JOGO DUROU 24 HORA(S)\n";
+	}
+	else if(a>b)
+	{
+		cout<<"O JOGO DUROU "<<(24-a)+b<<" HORA(S)\n";
+	}
+	else if(a<b)
+	{
+		cout<<"O JOGO DUROU "<<b-a<<" HORA(S)\n";
+	}
 }
