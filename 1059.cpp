ab2755d0e22814f5009bd63a7ff549c4fb4f1386
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 21:57:24 +0600

    Create 1059.cpp

---
+++ 1059.cpp
@@ -0,0 +1,15 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=2; i<=100; i+=2)
+    {
+        cout<<i<<endl;
+    }
+}
