b8e725a5ab97eba0c15ddb533ebc51df6dddf037
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 21:53:17 +0600

    Update 1047.cpp

--- 1047.cpp
+++ 1047.cpp
@@ -1,8 +1,42 @@
-#include<iostream>
-#include<stdio.h>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+    /*int h,h1,m,m1;
+    cin>>h>>m>>h1>>m1;
+    if(((h==0 && h1==0) && (m==0 &&m1==0)) || ((h==h1) && (m=m1)))
+    {
+        cout<<"O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n";
+    }
+    else if(m<m1)
+    {
+        if(h>h1)//8,9-7,10
+        {
+            cout<<"O JOGO DUROU "<<(24-h)+h1<<" HORA(S) E "<<m1-m<<" MINUTO(S)\n";
+        }
+        else
+        {
+            cout<<"O JOGO DUROU "<<h1-h<<" HORA(S) E "<<m1-m<<" MINUTO(S)\n";
+        }
+    }
+    else if(m>m1)
+    {
+        if(h>h1)//8,9-7,10
+        {
+            cout<<"O JOGO DUROU "<<((24-h)+h1)-1<<" HORA(S) E "<<(60-m)+m1<<" MINUTO(S)\n";
+        }
+        else
+        {
+            cout<<"O JOGO DUROU "<<(h1-h)-1<<" HORA(S) E "<<(60-m)+m1<<" MINUTO(S)\n";
+        }
+    }*/
+
     int a,b,c,d,x,y;
     cin>>a>>x>>b>>y;
     if(a==y && b==y && y==x){
