10ac7fcf8a5cd4ed22ca3598870bea4b4cd004b0
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 17:58:38 +0600

    Update 1043.cpp

--- 1043.cpp
+++ 1043.cpp
@@ -1,16 +1,25 @@
-#include<bits/stdc++.h>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<iostream>
 using namespace std;
 int main()
 {
     float a,b,c;
     cin>>a>>b>>c;
-    if((a+b>c)&&(a+c>b)&&(c+b>a))
+    if((a+b>c)&&(a+c>b &&(b+c>a)))
     {
         printf("Perimetro = %.1f\n",a+b+c);
     }
-    else{
+    else
+    {
         a=((a+b)*c)/2;
         printf("Area = %.1f\n",a);
     }
     return 0;
+
+
 }
