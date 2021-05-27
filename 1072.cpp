d5a63d8b8623a636879ba7e2f99ffffc6c94d8b6
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 22:24:01 +0600

    Update 1072.cpp

--- 1072.cpp
+++ 1072.cpp
@@ -1,21 +1,32 @@
-#include<iostream>
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include <iostream>
+
 using namespace std;
-int main()
+
+int main(int argc, const char * argv[])
 {
-    int t,n;
-    int r=0,R=0;
-    cin>>t;
-    for(int i=1;i<=t;i++)
-    {
-        cin>>n;
-        if(n>=10&&n<=20)
-        {
-            r++;
-        }
-        else
-        R++;
-    }
-    cout<<r<<" in\n";
-    cout<<R<<" out\n";
-    return 0;
+	int n, i;
+	long int num;
+	long int in = 0, out = 0;
+
+	cin >> n;
+
+	for (i = 0; i < n; i++)
+	{
+		cin >> num;
+		if(num >= 10 && num <= 20)
+			in++;
+		else
+			out++;
+	}
+
+	cout << in << " in" << endl << out << " out" << endl;
+
+	return 0;
 }
+
