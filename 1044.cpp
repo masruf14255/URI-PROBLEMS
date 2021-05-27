ed16abbfb21b7d67f1b0e150864a68d506ce7efc
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 19:49:15 +0600

    Update 1044.cpp

--- 1044.cpp
+++ 1044.cpp
@@ -1,14 +1,21 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    a=abs(a);
-    b=abs(b);
-    if(b>a)swap(a,b);
-    if(a%b==0)cout<<"Sao Multiplos\n";
-    else
-        cout<<"Nao sao Multiplos\n";
-    return 0;
+	int a,b;
+	cin>>a>>b;
+	if(max(a,b)%min(a,b)==0)
+	{
+		cout<<"Sao Multiplos\n";
+	}
+	else
+	{
+		cout<<"Nao sao Multiplos\n";
+	}
 }
