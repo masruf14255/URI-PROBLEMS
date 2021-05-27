7f722a0c134dc832d26a4a7474dfde96499a7336
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-05-27 19:53:54 +0600

    Update 1044.cpp

--- 1044.cpp
+++ 1044.cpp
@@ -8,14 +8,37 @@
 using namespace std;
 int main()
 {
-	int a,b;
-	cin>>a>>b;
-	if(max(a,b)%min(a,b)==0)
+	double a,b,c,d,Media,n,avg;
+	cin>>a>>b>>c>>d;
+	a=a*2;
+	b=b*3;
+	c=c*4;
+	d=d*1;
+	Media =(a+b+c+d)/10.0;
+	printf("Media: %.1lf\n",Media);
+	if(Media>=7.0)
 	{
-		cout<<"Sao Multiplos\n";
+		printf("Aluno aprovado.\n");
 	}
-	else
+	else if(Media<5)
 	{
-		cout<<"Nao sao Multiplos\n";
+		printf("Aluno reprovado.\n");
+	}
+	else if(Media>=5.0 && Media<=5.9)
+	{
+		printf("Aluno em exame.\n");
+		cin>>n;
+		printf("Nota do exame: %.1lf\n",n);
+		avg = (Media+n)/2;
+
+		if(avg>=5.0)
+		{
+			printf("Aluno aprovado.\n");
+		}
+		else if(avg<=4.9)
+		{
+			printf("Aluno reprovado.\n");
+		}
+		printf("Media final: %.1lf\n",avg);
 	}
 }
