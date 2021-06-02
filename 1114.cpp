889e2cce9b3cccbee42236c375de937e622b9748
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 15:15:04 +0600

    Create 1114.cpp

---
+++ 1114.cpp
@@ -0,0 +1,22 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	while(cin>>n)
+	{
+		if(n==2002)
+		{
+			cout<<"Acesso Permitido"<<endl;
+			break;
+		}
+		else
+			cout<<"Senha Invalida"<<endl;
+	}
+}
