f7947d8399b5a7c083c5c14114729d136650ff7a
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 09:24:40 +0600

    Create 1078.cpp

---
+++ 1078.cpp
@@ -0,0 +1,18 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=10; i++)
+    {
+        printf("%d x %d = %d\n",i,n,i*n);
+    }
+}
+
