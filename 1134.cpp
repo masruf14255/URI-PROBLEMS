e00eb7942804912f05c892d159fd7ae24319640b
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 18:59:17 +0600

    Create 1134.cpp

---
+++ 1134.cpp
@@ -0,0 +1,29 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,m=0,n=0,k=0;
+    while(1)
+    {
+        cin>>a;
+        if(a==1)
+        m++;
+        if(a==2)
+        n++;
+        if(a==3)
+        k++;
+        if(a==4)
+        break;
+
+
+    }cout<<"MUITO OBRIGADO\n";
+    cout<<"Alcool: "<<m<<endl;
+    cout<<"Gasolina: "<<n<<endl;
+    cout<<"Diesel: "<<k<<endl;
+}
