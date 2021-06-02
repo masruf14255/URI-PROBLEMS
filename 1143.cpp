5972a8d3657881c56f3f3915c23b1892bc92fa5c
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 19:02:44 +0600

    Create 1143.cpp

---
+++ 1143.cpp
@@ -0,0 +1,23 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+int main(int argc, const char * argv[])
+
+{
+    int x, i;
+
+    cin >> x;
+
+    for(i = 1; i <= x; i++){
+        cout << i << " " << i*i << " " << i*i*i << endl;
+    }
+
+    return 0;
+}
