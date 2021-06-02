c0b83eb68354a54571f27a766e3d648c1684598e
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 14:54:28 +0600

    Create 1095.cpp

---
+++ 1095.cpp
@@ -0,0 +1,16 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,j;
+	for(int i=1,j=60; j>=0; i+=3,j-=5)
+	{
+		cout<<"I="<<i<<" "<<"J="<<j<<endl;
+	}
+}
