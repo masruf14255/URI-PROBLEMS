673c3bad3f51a4f69d0f546ce5b4c930efc1fe16
masruf14255 <74848229+masruf14255@users.noreply.github.com>
2021-06-02 14:59:00 +0600

    Create 1098.cpp

---
+++ 1098.cpp
@@ -0,0 +1,19 @@
+/*
+            Bismillahir Rahmanir Rahim
+            Read in the name of Allah, who created you!
+            Al Mashruf Tonoy
+            Department of CSE, Daffodil Internatinal University.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	float i,j;
+	for(float i=0; i<=2.2; i+=0.2)
+	{
+		for(float j=1; j<=3; j++)
+		{
+			cout<<"I="<<i<<" "<<"J="<<j+i<<endl;
+		}
+	}
+}
